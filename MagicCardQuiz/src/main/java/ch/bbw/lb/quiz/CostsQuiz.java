033426401057abab068cb72fd60bb21cc457c306
Darki002 <124689003+LlynBaum@users.noreply.github.com>
2024-01-12 14:33:34 +0100

    IQuit extended

--- MagicCardQuiz/src/main/java/ch/bbw/lb/quiz/CostsQuiz.java
+++ MagicCardQuiz/src/main/java/ch/bbw/lb/quiz/CostsQuiz.java
@@ -5,4 +5,24 @@ public class CostsQuiz implements IQuiz {
     public void start() {
         System.out.println("Costs quiz started");
     }
+
+    @Override
+    public void getNextQuestion() {
+
+    }
+
+    @Override
+    public void checkAnswer(String answer) {
+
+    }
+
+    @Override
+    public void end() {
+
+    }
+
+    @Override
+    public void getStatistics() {
+
+    }
 }
--- MagicCardQuiz/src/main/java/ch/bbw/lb/quiz/IQuiz.java
+++ MagicCardQuiz/src/main/java/ch/bbw/lb/quiz/IQuiz.java
@@ -3,5 +3,8 @@ package ch.bbw.lb.quiz;
 public interface IQuiz {
 
     void start();
-
+    void getNextQuestion();
+    void checkAnswer(String answer);
+    void end();
+    void getStatistics();
 }
