43d608e5eb8dcebe1ca384194e41467e8321556d
Llyn Baumann <124689003+LlynBaum@users.noreply.github.com>
2024-01-18 09:32:53 +0100

    Update StatisticsQueryHandler.java

--- MagicCardQuiz/src/main/java/ch/bbw/lb/db/StatisticsQueryHandler.java
+++ MagicCardQuiz/src/main/java/ch/bbw/lb/db/StatisticsQueryHandler.java
@@ -1,10 +1,21 @@
 package ch.bbw.lb.db;
 
+import org.bson.Document;
+
 public class StatisticsQueryHandler extends QueryHandlerBase {
 
     private final String userName;
 
     public StatisticsQueryHandler(String userName) {
         this.userName = userName;
+    }
+
+    public void saveGameResult(int correctAnswers, int wrongAnswers) {
+        var collection = initMongoClient("statistics");
+        var document = new Document();
+        document.append("username", userName);
+        document.append("correctAnswers", correctAnswers);
+        document.append("wrongAnswers", wrongAnswers);
+        collection.insertOne(document);
     }
 }
